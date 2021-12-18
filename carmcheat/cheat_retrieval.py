6e21b2b204860d66b3232b77390b45193ada0c5a
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-12-18 06:23:50 +0100

    Allow checking intermediate values ==> allow using longer lengths

--- carmcheat/cheat_retrieval.py
+++ carmcheat/cheat_retrieval.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
 import argparse
+import sys
 import z3
 
-from carmcheat.algorithm import hash2str, str2hash, cheat_length_range
+from carmcheat.algorithm import hash2str, str2hash, cheat_length_range, hash_init, hash_update, hash_final
 
 
 def char_to_val(x):
     return ord(x.lower()) - ord("a") + 22
 
 
-def step(code, code2, sum, inp):
+def z3_hash_step(code1, code2, sum, inp):
     sum += inp
-    code += (inp << 11)
-    code = z3.LShR(code, 17) + (code << 4)
+    code1 += (inp << 11)
+    code = z3.LShR(code1, 17) + (code1 << 4)
     code2 = z3.LShR(code2, 29) + (code2 << 3) + inp * inp
     return code, code2, sum
 
 
-def final(code, code2, sum):
-    return z3.LShR(code, 11) + (sum << 21), code2
+def z3_hash_final(code1, code2, sum):
+    return z3.LShR(code1, 11) + (sum << 21), code2
 
 
 def recursive_crib_iterator(result, remaining_cribs):
@@ -27,31 +28,69 @@ def recursive_crib_iterator(result, remaining_cribs):
         return result
 
 
-def run(target_hash, length, crib, database, database_filename=None):
+def run(target_hash, length, crib, database, database_filename=None, check_intermediates=True, force=False):
+    min_length, max_length = cheat_length_range(target_hash)
     for offset in range(length - len(crib) + 1):
-        keycodes = [z3.BitVec(f"k_{i}", 32) for i in range(length)]
         s = z3.Solver()
 
-        # All characters are [a-z]
+        # Create Z3 keycode objects
+        # FIXME: Their range is [22, 22+26]=[22,48], will reducing their bit width to 6 make the algorithm faster?
+        keycodes = [z3.BitVec(f"k_{i}", 32) for i in range(length)]
+
+        # All characters are 22 + [a-z]
         for k in keycodes:
             s.add(k >= 22)
             s.add(k < 22 + 26)
 
-        # Apply the Carmageddon hashing algorithm on the keycodes
-        code = code2 = sum = z3.BitVecVal(0, 32)
-        for k in keycodes:
-            code, code2, sum = step(code, code2, sum, k)
+        # Apply the "crib" to the input characters
         for c, k in zip(crib, keycodes[offset:]):
             s.add(k == char_to_val(c))
-        code, code2 = final(code, code2, sum)
-        s.add(code == target_hash[0])
-        s.add(code2 == target_hash[1])
+
+        # Exclude all items from the database
         for dbitem in database:
             s.add(z3.Or(*[k != char_to_val(dbitem[ki]) for ki, k in enumerate(keycodes)]))
+
+        # Store all intermediate hash results in this list
+        intermediates = []
+
+        # Apply the Carmageddon hashing algorithm to the keycodes
+        code1 = code2 = sum = z3.BitVecVal(0, 32)
+        for k_i, k in enumerate(keycodes, 1):
+            # One step of the hash
+            code1, code2, sum = z3_hash_step(code1, code2, sum, k)
+            if check_intermediates and (force or min_length <= k_i <= max_length):
+                # Calculate the final hash for each intermediate key code ==> string is shorter
+                final = z3_hash_final(code1, code2, sum)
+                intermediates.append(final)
+
+        if intermediates:
+            # Generate a success when an intermediate hash matches the target hash
+            s.add(z3.Or(*[z3.And(intermediate[0] == target_hash[0], intermediate[1] == target_hash[1]) for intermediate in intermediates]))
+        else:
+            # Generate a success when the final hash matches the target hash
+            final = z3_hash_final(code1, code2, sum)
+            s.add(z3.And(final[0] == target_hash[0], final[1] == target_hash[1]))
+
         while (t := s.check()) == z3.sat:
-            cleartext = "".join(chr(s.model()[k].as_long() + ord('a') - 22) for k in keycodes)
+            full_cleartext = "".join(chr(s.model()[k].as_long() + ord('a') - 22) for k in keycodes)
+            if check_intermediates:
+                state = hash_init()
+                for c_i, char in enumerate(full_cleartext, 1):
+                    state = hash_update(*state, char)
+                    final = hash_final(*state)
+                    if final == target_hash:
+                        cleartext = full_cleartext[:c_i]
+                        excluded_keycodes = [k for k in keycodes[:c_i]]
+                        break
+                else:
+                    print("Error: no intermediate hash matches the target hash!", file=sys.stderr)
+                    cleartext = full_cleartext
+                    excluded_keycodes = keycodes
+            else:
+                cleartext = full_cleartext
+                excluded_keycodes = keycodes
+            s.add(z3.Or(*[k != s.model()[k].as_long() for k in excluded_keycodes]))
             print(f"{hash2str(target_hash)}:{cleartext}")
-            s.add(z3.Or(*[k != s.model()[k].as_long() for k in keycodes]))
             if database_filename:
                 with open(database_filename, "a") as fn:
                     fn.write(f"{hash2str(target_hash)}:{cleartext}\n")
@@ -63,9 +102,10 @@ def main():
     parser = argparse.ArgumentParser(allow_abbrev=False,
                                      description="Use SAT solver to find cheat codes hashing to a target")
     parser.add_argument("target", help="Target hash (format=XXXXXXXX:XXXXXXXX), where X is hexadecimal")
-    parser.add_argument("lengths", nargs=argparse.ONE_OR_MORE, type=int, help="Length(s) of cheat code to check/find")
+    parser.add_argument("length", type=int, help="Length(s) of cheat code to check/find")
     parser.add_argument("--crib", default="", help="Find only cheat codes that contain the 'crib'")
     parser.add_argument("--database", metavar="DB", help="Append found cheat codes to this database")
+    parser.add_argument("--intermediates", action="store_true", help="Match intermediates")
     parser.add_argument("--force", action="store_true", help="Force search")
     args = parser.parse_args()
 
@@ -95,13 +135,13 @@ def main():
         for entry in database:
             print(f"{hash_target_str}:{entry}")
 
-    for length in args.lengths:
-        if not args.force and not (cheat_min_length <= length <= cheat_max_length):
-            print(f"Length={length} is out of range [{cheat_min_length},{cheat_max_length}]: skipping search (use --force to override)")
-            continue
+    if not args.force:
+        if not args.intermediates and not cheat_min_length < args.length < cheat_max_length:
+            print(f"Length={args.length} is out of range [{cheat_min_length},{cheat_max_length}] => skipping search (use --force to override)")
+            return
 
-        print(f"Checking length={length}")
-        run(target_hash, length, args.crib, database, database_filename=args.database)
+    run(target_hash, args.length, args.crib, database, database_filename=args.database,
+        check_intermediates=args.intermediates, force=args.force)
 
     print(f"Search finished")
 
